22af64d5211f191970dbd2cd9399c81038558db4
HollyJHuber <hollyjhuber@gmail.com>
2018-06-08 14:49:34 -0400

    adds JavaScript & jQuery

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,21 @@
 
-const selectReligion = document.getElementById('religion');
-selectReligion.onclick = selectIt;
+// document ready wrapper tells scripts to wait until DOM has loaded
+$(function() {
 
-function selectIt(){
-	selectReligion.style.color = "orangered";
-}
+
+// javascript method
+	const selectReligion = document.getElementById('religion');
+	const selectReligion2 = document.getElementById('religion2');
+	selectReligion.onclick = selectIt;
+
+	function selectIt(){
+		selectReligion.style.color = "orangered";
+		selectReligion.style.fontWeight = "700";
+
+	//jquery methods
+		$("#religion2").css("display", "inline").delay(800).fadeIn(1200);
+		$(".fa-church").css("color", "orangered");
+
+	}
+
+});
