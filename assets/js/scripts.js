050b3f70cfe4e0e6c0c9e6b182703389a4b5f4cf
HollyJHuber <hollyjhuber@gmail.com>
2018-06-08 20:20:40 -0400

    handles WHAT2 clicks

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,17 +2,35 @@
 // document ready wrapper tells scripts to wait until DOM has loaded
 $(function() {
 
+	var currentWhat1;
+	var currentWhat2;
+
+
+	// capturing what1 clicks
 	$(document).on('click', '.what1', function(){
 
-		var currentId = $(this).attr('id');
+		currentWhat1 = $(this).attr('id');
 
-		$(this).toggleClass('show');
-		$('.what1.show .col2 .what2').slideToggle();
+		$(this).toggleClass('expand');
 
-		$('.what1.show').each(function(i){
-			if($(this).attr('id') != currentId){
-				$(this).toggleClass('show');
+		$('.what1.expand .col2 .what2').slideToggle();
+
+		$('.what1.expand').each(function(i){
+			if($(this).attr('id') != currentWhat1){
+				$(this).toggleClass('expand');
 			}
 		});
+	});
+
+
+	// capturing what2 clicks
+	$(document).on('click', '.what2 li', function(){
+
+		currentWhat2 = $(this).attr('id');
+		
+		$('#notation').replaceWith(currentWhat1 + " > " + currentWhat2);
+		$('.what1').remove();
+
+
 	});
 });
