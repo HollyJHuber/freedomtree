767d34785a36d370a251aa4672a71603c574fa43
HollyJHuber <hollyjhuber@gmail.com>
2018-06-10 12:02:06 -0400

    inserts what3 content for test issues

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,7 +7,7 @@ $(function() {
 	const choices = [];
 
 
-	// capturing what1 clicks
+	// capturing what1 clicks, display what2 content
 	$('.what1').on('click', function(){
 
 		currentWhat1 = $(this).attr('id');
@@ -24,7 +24,7 @@ $(function() {
 	});
 
 
-	// capturing what2 clicks
+	// capturing what2 clicks, display what3 content
 	$('.what2 li').on('click', function(){
 
 		currentWhat2 = $(this).attr('id');
@@ -32,14 +32,35 @@ $(function() {
 		$('#question').text(currentWhat2);
 		
 		// #path is text for now, will be links
-		$('#path').replaceWith(" Issues > " + currentWhat1);
+		$('#path').text(" Issues > " + currentWhat1);
+
 		// write to an array
 		choices.push(currentWhat1, currentWhat2);
 
 		$('.what1').remove();
-		// insert what3
-		// use replaceWith()??
 
+		if (currentWhat1=="Religion"){
+			//insert prayer text
+			what3Text = [	'I do not wish to participate in prayer',
+							'I am being forced to pray',
+							'I am being forced listen silently to prayer',
+							'I am required to be present while others pray',
+							'Other'];
+
+		} else {
+			//insert pledge text
+			what3Text = [	'I do not wish to participate',
+							'I am being forced to recite',
+							'I am being forced to stand',
+							'I am being treated unfairly for not participating',
+							'I do not wish to say "Under God”',
+							'Other'];
+		}
+
+		what3Text.forEach(function(value){
+			let newWhat3 = '<div class="what3"><div class="listing">' + value + '</div></div>';
+			$("#instruction").append(newWhat3)
+		});
 
 	});
 });
