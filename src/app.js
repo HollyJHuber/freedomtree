ef6cd8b71efc62c8c06a4bee3a131b17cf578c10
HollyJHuber <hollyjhuber@gmail.com>
2018-06-27 12:47:46 -0400

    adds npm modules, validator, react, react-dom, babel

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('app.js is running!!! WOO HOO!!');
+import validator from 'validator';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { square, cube } from './utils.js';
+
+// console.log('app.js is running!!! WOO HOO!!');
+// console.log('is email? ' + validator.isEmail('test'));
+
+const template = <p>Babel and React are running</p>;
+ReactDOM.render(template, document.getElementById('app'));
---
+++ src/utils.js
@@ -0,0 +1,6 @@
+console.log('utils.js is running!');
+
+const square = (x) => x * x;
+const cube = (x) => x * x * x;
+
+export { square, cube };
