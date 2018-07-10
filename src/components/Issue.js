66e95c4602385b7c7bbb5fff20523ebfa6e8aaa4
HollyJHuber <hollyjhuber@gmail.com>
2018-07-10 12:20:39 -0400

    adds array of objects for data display

--- src/components/Issue.js
+++ src/components/Issue.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Issue = (props) => (
-	<div 
-		className = "what1">
-		<div className="col1"><i className="fas fa-church"></i></div>
-		<div className="col2">Religion</div>
-	</div>
-);
-
-
+class Issue extends React.Component {
+	render() {
+		return(
+			<div className ="what1">
+				<div className="col1"><i className={this.props.issueIcon}></i></div>
+				<div className="col2">{this.props.issueText}</div>
+			</div>
+		);
+	}
+}
 
 export default Issue;
--- src/components/Issues.js
+++ src/components/Issues.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Issue from './Issue';
 
-const Issues =(props) => (
-  <div>
-    <h1 id="question">What's Happening?</h1>
-    <h4 id="instruction">Select the issue that best describes your situtation:</h4>
-		<Issue />
-	</div>
-);
+class Issues extends React.Component {
+  render() {
+
+      return (
+      <div>
+        <h1 id="question">What's Happening?</h1>
+        <h4 id="instruction">Select the issue that best describes your situtation:</h4>
+        {
+          this.props.issues.map((issue) => 
+            <Issue key={issue.id} issueIcon={issue.icon} issueText={issue.text}/>)
+        }
+        
+      </div>
+    );
+  }
+}
 
 export default Issues;
