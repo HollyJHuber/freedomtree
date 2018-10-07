5e144d2c10602a7354b73f21a2ca45be692e7f7b
HollyJHuber <hollyjhuber@gmail.com>
2018-10-07 15:41:07 -0400

    change firebase values to variables

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,16 +1,30 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyBp71CRQn7dppg_BJZJ9BF1vuvmLXoyJJE",
-  authDomain: "freedomtree-4bc52.firebaseapp.com",
-  databaseURL: "https://freedomtree-4bc52.firebaseio.com",
-  projectId: "freedomtree-4bc52",
-  storageBucket: "freedomtree-4bc52.appspot.com",
-  messagingSenderId: "429956210659"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
+
+// temp code to ensure firebase is hooked up!
+database.ref('expenses')
+.once('value')
+.then((snapshot) => {
+  const expenses = []; // create a new array
+  snapshot.forEach((childSnapshot) => { // iterate over each object in snapshot
+    expenses.push({ // push the objects to the expenses array
+      id: childSnapshot.key, // set id to the auto-generated unique id (here's how we get it)
+      ...childSnapshot.val() // spread out whatever other data is in the object
+    });
+  });
+    console.log(expenses); // let's see what our transformed data looks like
+});
 
 export { firebase, database as default };
