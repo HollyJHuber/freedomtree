500dce1243e09347118ff258df8d3c1c704bf5e7
HollyJHuber <hollyjhuber@gmail.com>
2018-11-09 09:49:46 -0500

    adds menu dropdown for minimized menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,38 +8,49 @@ class NavBar extends React.Component {
 		menuOption: undefined
 	};
 
-handleMenuModal = (displayMenu) => {
-	this.setState(() => ({menuOption: displayMenu}));
-};
-handleCloseMenuModal = () => {
-	this.setState(() => ({menuOption: undefined}))
-};
+	handleMenuModal = (displayMenu) => {
+		document.getElementById("menuDropdown").classList.toggle("show");
+		this.setState(() => ({menuOption: displayMenu}));
+	};
+	handleCloseMenuModal = () => {
+		this.setState(() => ({menuOption: undefined}))
+	};
 
-render() {
-	return (
-		<header>
-			<nav id="top" className={this.props.landingPage ? "nav__barLandingPage" : "nav__bar"}>
-				<Link to="/">
-					<span className="nav__item">
-						<img className="nav__logo" src="./assets/img/FreedomTreeAppIcon.svg" alt="tree icon"/>
-						<img className="nav__logoText" src="./assets/img/FreedomTreeLogoTextOnly.svg" alt="Freedom Tree"/>
-					</span>
-				</Link>
-				<ul className="nav__menu">
-					<li className="nav__item nav__itemLg"><a onClick = {() => this.handleMenuModal("about")}>About</a></li>
-					<li className="nav__item nav__itemLg"><a onClick = {() => this.handleMenuModal("contact")}>Contact</a></li>
-					<li className="nav__item nav__itemLg"><a onClick = {() => this.handleMenuModal("donate")}>Donate</a></li>
-					<li className="nav__item nav__itemLg"><a onClick = {() => this.handleMenuModal("faq")}>FAQ</a></li>
-					<li className="nav__item nav__itemSm"><a href="#"><i className="fas fa-bars fa-2x"></i></a></li>
-				</ul>
-			</nav>
-			<MenuModal 
-				menuOption = {this.state.menuOption}
-				handleCloseMenuModal = {this.handleCloseMenuModal}
-			/>
-		</header>
-	)
-}
+	dropDownMenu = () => {
+		document.getElementById("menuDropdown").classList.toggle("show");
+	}
+
+	render() {
+		return (
+			<header>
+				<nav id="top" className={this.props.landingPage ? "nav__barLandingPage" : "nav__bar"}>
+					<Link to="/">
+						<span className="nav__item">
+							<img className="nav__logo" src="./assets/img/FreedomTreeAppIcon.svg" alt="tree icon"/>
+							<img className="nav__logoText" src="./assets/img/FreedomTreeLogoTextOnly.svg" alt="Freedom Tree"/>
+						</span>
+					</Link>
+					<ul className="nav__menu">
+						<li className="nav__item nav__itemLg"><a onClick = {() => this.handleMenuModal("about")}>About</a></li>
+						<li className="nav__item nav__itemLg"><a onClick = {() => this.handleMenuModal("contact")}>Contact</a></li>
+						<li className="nav__item nav__itemLg"><a onClick = {() => this.handleMenuModal("donate")}>Donate</a></li>
+						<div className="nav__dropdown">
+							<li className="nav__item nav__itemSm"><a onClick = {() => this.dropDownMenu()}><i className="fas fa-bars fa-2x"></i></a></li>
+							<div id="menuDropdown" className="nav__dropdown__content">
+							<li className="nav__item nav__itemSm"><a onClick = {() => this.handleMenuModal("about")}>About</a></li>
+							<li className="nav__item nav__itemSm"><a onClick = {() => this.handleMenuModal("contact")}>Contact</a></li>
+							<li className="nav__item nav__itemSm"><a onClick = {() => this.handleMenuModal("donate")}>Donate</a></li>
+							</div>
+						</div>
+					</ul>
+				</nav>
+				<MenuModal 
+					menuOption = {this.state.menuOption}
+					handleCloseMenuModal = {this.handleCloseMenuModal}
+				/>
+			</header>
+		)
+	}
 }
 
 export default NavBar;
